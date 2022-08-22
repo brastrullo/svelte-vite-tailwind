2ccd561043e0af6c12ed3149c4a6753f31193029
Bradley Rastrullo <11504992+brastrullo@users.noreply.github.com>
2022-08-22 15:10:24 -0400

    migrated from /svelte-snowpack

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,svelte}"],
+  purge: ['./src/**/**/**/*.{html,js,svelte}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: ['"Playfair Display"', 'serif'],
+      secondary: ['"Open Sans"', 'sans-serif'],
+    },
+    extend: {
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0, 1fr))',
+        8: 'repeat(8, minmax(0, 1fr))',
+        9: 'repeat(9, minmax(0, 1fr))',
+        10: 'repeat(10, minmax(0, 1fr))',
+        11: 'repeat(11, minmax(0, 1fr))',
+        12: 'repeat(12, minmax(0, 1fr))',
+      },
+    },
+  },
+  variants: {
+    animation: ['responsive', 'motion-safe', 'motion-reduce'],
+    extend: {
+      transitionProperty: ['hover', 'focus'],
+    },
   },
   plugins: [],
-}
+};
